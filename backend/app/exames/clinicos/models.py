14a78d865db7a9f7c2a2455854dc05e4e90d84fb
Joao Vitor <joaovitor.jaques.7748@gmail.com>
2026-02-26 01:43:51 -0300

    feat: model de dominio de exames clinicos/imagem

---
+++ backend/app/exames/clinicos/models.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, Integer, Boolean, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+
+# from app.database.connection import Base
+
+
+class ExamesClinicos(Base):
+    """ Tabela base de exames clínicos"""
+
+    __tablename__ = 'exames_clinicos'
+
+    exame_id = Column(Integer, primary_key=True, autoincrement=True)
+
+    ist = Column(Boolean, nullable=True)
+    ist_numero = Column(Integer, nullable=True)
+    ist_hiv = Column(Boolean, nullable=True)
+    ist_diagnosticadas_num = Column(Integer, nullable=True)
+
+    diagnostico_cancer = Column(Boolean, nullable=True)
+    diagnostico_cin = Column(Boolean, nullable=True)
+    diagnostico_hpv = Column(Boolean, nullable=True)
+    diagnostico = Column(Boolean, nullable=True)
+
+    hinselmann = Column(Boolean, nullable=True)
+    schiller = Column(Boolean, nullable=True)
+    citologia = Column(Boolean, nullable=True)
+    biopsia = Column(Boolean, nullable=True)
+
+    medicamento = relationship("Medicamento", back_populates="exame_clinico")   # Para Medicamentos se relacionarem a exames
+
+
+class ResultadoExameClinicoIa:
+    """ Tabela para persistir resultados de análise de exames clínicos pelo modelo """
+
+    __tablename__ = 'resultados_analise_exame_clinico_ia'
+
+    predicao_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    risco = Column(String(10), nullable=False)
+    data_analise = Column(DateTime, default=datetime.utcnow())
+
+    exame_clinico_id = Column(Integer, ForeignKey('exames_clinicos.exame_id'))              #  Relacionando o resultado da analise clinica com o exame clinico atraves de ForeignKey
+    exame_clinico = relationship("ExamesClinicos", back_populates="resultados")
+
+
+    #  Ela ainda n tem schemas ou classe pública, pois não possui dados sensíveis como senhas
+
---
+++ backend/app/exames/imagem/models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+
+#from app.database.connection import Base
+
+
+class ExameImagem(Base):
+    """ Tabela Base de Exames """
+
+    __tablename__ = 'exames_imagem'
+
+    exame_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    tipo = Column(String(45), nullable=False)
+    descricao = Column(String(45))
+    link_imagem = Column(String(45))
+
+    medicamento = relationship("Medicamento", back_populates="exame_imagem")            #  Para médicos se relacionarem com exames de imagem
+    upload_imagem = relationship("UploadExameImagem")    #  Para a lógica de 'Uploads' se relacionar com exames de imagem
+
+
+class UploadExameImagem(Base):
+    """ Taabela que guarda caminhos de "upload' de imagens """
+
+    __tablename__ = 'uploads_exame_imagem'
+
+    upload_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    titulo = Column(String(45), nullable=False)
+    caminho_upload = Column(String(255), nullable=False)
+
+    exame_imagem_id = Column(Integer, ForeignKey('exames_imagem.exame_id'))  # Relacionando a imagem de upload a tabela de exames de imagem com ForeignKey
+
+
+class ResultadoExameImagemIa(Base):
+    """ Tabela para salvar resultados de exames de imagem pelo modelo """
+
+    __tablename__ = 'resultados_exame_imagem_ia'
+
+    resultado_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    risco = Column(Float, nullable=False)
+    data_analise = Column(DateTime, default=datetime.utcnow)
+
+    exame_imagem_id = Column(Integer, ForeignKey('exames_imagem.exame_id'))             #  Relacionando o resultado da análise do exame de imagem com a imagem
+    exame_imagem = relationship("ExameImagem", back_populates="resultados")    #  Relacionando Resultados com exames de imagem
+
+    #  Ela ainda n tem schemas ou classe pública, pois não possui dados sensíveis como senhas
